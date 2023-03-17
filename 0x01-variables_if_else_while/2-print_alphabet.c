47c2530b9acdf3fd396c80dd544162a6129551e5
El firdaous <sokainaelfirdaous@gmail.com>
2023-03-17 13:50:50 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabetic
+ * Return: Alwyas (success)
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
