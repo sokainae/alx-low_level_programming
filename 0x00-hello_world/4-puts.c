dee3f1393264f8e1fbccef9afa0c935ed199643d
El firdaous <sokainaelfirdaous@gmail.com>
2023-03-17 04:28:11 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
 *main-main entry point
-*return:always 0 (sucess)
+*Return:always 0 (sucess)
 */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0);
+Return (0);
 }
