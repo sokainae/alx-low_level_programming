01330def8a21ea2984202a1036269eb1c2b13e50
El firdaous <sokainaelfirdaous@gmail.com>
2023-03-17 16:22:35 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int d;
+for (d = '0'; d <= '9'; d++)
+putchar(d);
+putchar('\n');
+return (0);
+}
